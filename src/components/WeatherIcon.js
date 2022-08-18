24280e3b1e09ae16d83bdbc3e8e9ea3fc119e28f
druckmax <max_sommerfeld@web.de>
2022-08-18 09:38:44 +0200

    Refactor WeatherIcon - refactors the useEffect in WeatherIcon   - move iconStorage in useEffect   - refactor method to find match using hasOwnProperty - cleans up unused dependencies in package.json - cleans up unused imports

--- src/components/WeatherIcon.js
+++ src/components/WeatherIcon.js
@@ -14,7 +14,6 @@ import {
 import { useEffect, useState } from "react";
 
 function WeatherIcon(props) {
-
   const [icon, setIcon] = useState(faSun);
   const [color, setColor] = useState("orange");
 
@@ -23,33 +22,35 @@ function WeatherIcon(props) {
     setColor(color);
   };
 
-  const iconStorage = [
-    { "01d": faSun, classColor: "orange" },
-    { "01n": faMoon, classColor: "lightGrey" },
-    { "02d": faCloudSun, classColor: "grey" },
-    { "02n": faCloudMoon, classColor: "darkGrey" },
-    { "03d": faCloud, classColor: "grey" },
-    { "03n": faCloud, classColor: "darkGrey" },
-    { "04d": faCloud, classColor: "grey" },
-    { "04n": faCloud, classColor: "darkGrey" },
-    { "09d": faCloudShowersHeavy, classColor: "blue" },
-    { "09n": faCloudShowersHeavy, classColor: "grey" },
-    { "10d": faCloudRain, classColor: "blue" },
-    { "10n": faCloudRain, classColor: "grey" },
-    { "11d": faCloudBolt, classColor: "red" },
-    { "11n": faCloudBolt, classColor: "red" },
-    { "13d": faSnowflake, classColor: "snowBlue" },
-    { "13n": faSnowflake, classColor: "snowBlue" },
-    { "50d": faSmog, classColor: "lightGrey" },
-    { "50n": faSmog, classColor: "darkGrey" },
-  ];
-
   useEffect(() => {
-    iconStorage.some((el, index) => {
-      const match = Object.keys(el)[0].includes(props.weather);
-      return match && setHandler(el[props.weather], el.classColor);
+    const iconStorage = [
+      { "01d": faSun, classColor: "orange" },
+      { "01n": faMoon, classColor: "lightGrey" },
+      { "02d": faCloudSun, classColor: "orange" },
+      { "02n": faCloudMoon, classColor: "darkGrey" },
+      { "03d": faCloud, classColor: "grey" },
+      { "03n": faCloud, classColor: "darkGrey" },
+      { "04d": faCloud, classColor: "grey" },
+      { "04n": faCloud, classColor: "darkGrey" },
+      { "09d": faCloudShowersHeavy, classColor: "blue" },
+      { "09n": faCloudShowersHeavy, classColor: "grey" },
+      { "10d": faCloudRain, classColor: "blue" },
+      { "10n": faCloudRain, classColor: "grey" },
+      { "11d": faCloudBolt, classColor: "red" },
+      { "11n": faCloudBolt, classColor: "red" },
+      { "13d": faSnowflake, classColor: "snowBlue" },
+      { "13n": faSnowflake, classColor: "snowBlue" },
+      { "50d": faSmog, classColor: "lightGrey" },
+      { "50n": faSmog, classColor: "darkGrey" },
+    ];
+
+    iconStorage.find((el) => {
+      return (
+        el.hasOwnProperty(props.weather) &&
+        setHandler(el[props.weather], el.classColor)
+      );
     });
-  }, [props.data, icon, color]);
+  }, [props.weather]);
 
   return (
     <div>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
-import "es6-promise/auto";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
